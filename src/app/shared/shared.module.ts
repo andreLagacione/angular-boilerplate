01d440371d145c83a44161ba68628bbf32be545b
andreLagacione <lagacioneandre@gmail.com>
2019-09-08 18:36:33 -0300

    add new components/modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler/src/core';
+
+// classes
+import { ErrorInterceptor } from '../security/auth/http-request-interceptor';
+
+// modules
+import { ButtonsModule } from '../buttons/buttons.module';
 
 @NgModule({
   declarations: [],
+  exports: [
+    ButtonsModule
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonsModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    }
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class SharedModule { }
