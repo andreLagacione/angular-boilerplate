f85f2edf33c3155179fbf644e5b884655e8fbda3
andreLagacione <lagacioneandre@gmail.com>
2020-01-12 17:29:44 -0300

    add new prop

--- src/app/shared/models/sort.model.ts
+++ src/app/shared/models/sort.model.ts
@@ -4,6 +4,7 @@ import { BaseResourceModel } from './base-resource.model';
 export class SortModel extends BaseResourceModel {
 	public unsorted?: boolean;
 	public sorted?: boolean;
+	public empty?: boolean;
 
 	constructor() {
 		super();
